1ca0d98481cf783d132629ef1cc92a2233200970
Brian MacPherson <brimac1634@gmail.com>
2020-10-29 18:24:12 +0800

    changed to using Future builder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -5,34 +5,31 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   static const pathName = '/orders';
 
   @override
-  _OrdersPageState createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<Orders>(context, listen: false).fetchOrders();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: ListView.builder(
-        itemBuilder: (ctx, i) {
-          return OrderItem(orderData.orders[i]);
-        },
-        itemCount: orderData.orders.length,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+            future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+            builder: (ctx, dataSnapshot) {
+              if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return Consumer<Orders>(
+                    builder: (ctx, orderData, child) => ListView.builder(
+                          itemBuilder: (ctx, i) {
+                            return OrderItem(orderData.orders[i]);
+                          },
+                          itemCount: orderData.orders.length,
+                        ));
+              }
+            }));
   }
 }
